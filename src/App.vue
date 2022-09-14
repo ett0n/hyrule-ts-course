3f4090a63965aa6ed58ca806154bc80a6ca43002
ett0n <mathieu.notte6@gmail.com>
2022-09-14 07:06:56 +0200

    f-commit

---
+++ src/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <button @click="handleClick('title')">order by title</button>
+  <button @click="handleClick('salary')">order by salary</button>
+  <button @click="handleClick('location')">order by location</button>
+  <JobList :jobs="jobs" :order="order"></JobList>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+import type Job from "./types/Job";
+import JobList from "./components/JobList.vue";
+import type OrderTerm from "./types/OrderTerm";
+const jobs = ref<Job[]>([
+  { title: "farm worker", location: "lon lon ranch", salary: 3000, id: "1" },
+  { title: "quarry man", location: "perwez", salary: 4000, id: "2" },
+  { title: "flute player", location: "flute land", salary: 6000, id: "3" },
+  { title: "dalton", location: "east valley", salary: 7000, id: "4" },
+]);
+
+const order = ref<OrderTerm>("title");
+
+const handleClick = (term: OrderTerm) => {
+  order.value = term;
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/JobList.vue
@@ -0,0 +1,75 @@
+<template>
+  <div class="job-list">
+    <p>Ordered by {{ order }}</p>
+    <transition-group name="list" tag="ul">
+      <li v-for="job in orderedJobs" :key="job.id">
+        <h2>{{ job.title }} in {{ job.location }}</h2>
+        <div class="salary">
+          <p>💎 {{ job.salary }} rupees</p>
+        </div>
+        <div class="description">
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Facere repudiandae, officiis nobis quibusdam recusandae omnis dolores?</p>
+        </div>
+      </li>
+    </transition-group>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { computed } from "vue";
+import type { PropType } from "vue";
+import type Job from "@/types/Job";
+import type OrderTerm from "@/types/OrderTerm";
+
+const props = defineProps({
+  jobs: {
+    required: true,
+    type: Array as PropType<Job[]>,
+  },
+  order: {
+    required: true,
+    type: String as PropType<OrderTerm>,
+  },
+});
+
+const orderedJobs = computed(() => {
+  return [...props.jobs].sort((a: Job, b: Job) => {
+    return a[props.order] > b[props.order] ? 1 : -1;
+  });
+});
+</script>
+<style scoped>
+.job-list {
+  max-width: 960px;
+  margin: 40px auto;
+}
+.job-list ul {
+  padding: 0;
+}
+.job-list li {
+  list-style-type: none;
+  background: white;
+  padding: 16px;
+  margin: 16px 0;
+  border-radius: 4px;
+}
+.job-list h2 {
+  margin: 0 0 10px;
+  text-transform: capitalize;
+}
+.salary {
+  display: flex;
+}
+.salary img {
+  width: 30px;
+}
+.salary p {
+  color: #17bf66;
+  font-weight: bold;
+  margin: 10px 4px;
+}
+
+.list-move {
+  transition: all 1s;
+}
+</style>
